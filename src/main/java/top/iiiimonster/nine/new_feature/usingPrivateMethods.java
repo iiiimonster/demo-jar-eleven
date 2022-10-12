5a1f2a55903d55a5c00126724d86e262350667cb
zwy <xiaoyang@heypad.cn>
2022-10-12 15:04:34 +0800

    JDK9 - 允许在接口中使用私有方法

--- src/main/java/top/iiiimonster/nine/new_feature/usingPrivateMethods.java
+++ src/main/java/top/iiiimonster/nine/new_feature/usingPrivateMethods.java
@@ -1,7 +1,24 @@
-package top.iiiimonster.nine.new_feature;/** 
-* @Description: TODO
-* @author: zwy
-* @date: 2022年10月12日 14:51
-*/
-public class usingPrivateMethods {
+package top.iiiimonster.nine.new_feature;
+
+/**
+ * &#064;Description:  JDK9 - 允许在接口中使用私有方法 <BR/>
+ * &#064;author:  zwy<BR/>
+ * &#064;date:  2022年10月12日 14:51<BR/>
+ */
+public class usingPrivateMethods implements usingPrivateMethodsInterface{
+    public static void main(String[] args) {
+        usingPrivateMethods usingPrivateMethods = new usingPrivateMethods();
+        System.out.println(usingPrivateMethods.sayHiBypublic());
+    }
+}
+
+interface usingPrivateMethodsInterface{
+    private String sayHiBypricate(){
+        return "你好";
+    }
+
+    default String sayHiBypublic(){
+        return sayHiBypricate();
+    }
+
 }
